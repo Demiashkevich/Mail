c829f9e9d8cde427a1bfd30c46cbfda98f8248ed
Egor <demiashkevich@mail.ru>
2018-06-24 14:12:31 +0300

    Add controller impl for client

---
+++ rest-endpoint/src/main/java/com/dzemiashkevich/mail/ApplicationClientController.java
@@ -0,0 +1,23 @@
+package com.dzemiashkevich.mail;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class ApplicationClientController {
+
+    @Autowired
+    private ApplicationService applicationService;
+
+    @PostMapping(value = "/applications")
+    public ResponseEntity<ApplicationRestDto> createApplication(@RequestBody ApplicationRestDto application) {
+        return ResponseEntity.ok(applicationService.createApplication(application));
+    }
+
+    @GetMapping(value = "/applications/{applicationId}")
+    public ResponseEntity<StatusRestDto> readApplicationStatus(@PathVariable Long applicationId) {
+        return ResponseEntity.ok(applicationService.readApplicationStatus(applicationId));
+    }
+
+}
